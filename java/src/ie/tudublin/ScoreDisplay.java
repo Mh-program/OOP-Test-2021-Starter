a5e94f62bdb899279018c75fea8d7444e0641b4a
mh-program <mahim.hossain157@gmail.com>
2021-05-17 20:22:52 +0100

    Update ScoreDisplay.java

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -7,6 +7,57 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
+	
+	String[] ArrayList = {"D", "E", "F", "G", "A", "B", "c", "d"};
+	String[] Type = {"Crotchet", "Quaver"};
+	int[] duration = {1, 2};
+	
+	public void loadScore() {
+		
+		for(int i = 0; i < ArrayList.length; i++) {
+			
+			textAlign(CENTER, CENTER);
+			text(ArrayList[i]);
+		}
+		
+	}
+	
+	public void drawLines {
+		
+		strokeWeight(3);
+		line(10,75,400,75);
+		strokeWeight(3);
+		line(10,100,400,100);
+		strokeWeight(3);
+		line(10,125,400,125);
+		strokeWeight(3);
+		line(10,150,400,150);
+		strokeWeight(3);
+		line(10,175,400,175);
+		
+	}
+	
+	public void printScores() {
+		
+		for(int i = 0; i < ArrayList.length; i++) {
+			
+			println(ArrayList[i] + "\t" + duration[i]);
+		}
+		if (duration[] = 0) {
+			println("\t" + Type[0]);
+		}
+		else {
+			println("\t" + Type[1]);
+		}
+		
+	}
+	
+	public void Note() {
+		
+		private char note;
+		private int duration;
+	}
+	
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
@@ -24,11 +75,16 @@ public class ScoreDisplay extends PApplet
 	public void setup() 
 	{
 		
+		public char charAt(int index);
+		public static boolean isDigit(char ch);
+		printScores();
 	}
 
 	public void draw()
 	{
 		background(255);
+		drawLines();
+		loadScore();
 		
 	}
 
